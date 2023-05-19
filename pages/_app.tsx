94e2a45fc2813d556c35ad06691bf4707b2384ee
Roosa H <[sirkusraja@gmail.com]>
2023-05-20 01:10:45 +0200

    added favicon and head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Little NextJS Project</title>
+        <meta name="description" content="Little NextJS Project" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
